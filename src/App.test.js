fc2b3b354071561b513d1f9118b30b66d18b91b4
kirank215 <k.kiran_98@yahoo.in>
2019-10-07 02:12:26 +0200

    Readme update

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,8 @@ import App from './components/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  ReactDOM.render(<Provider>, div);
   ReactDOM.render(<App />, div);
+  ReactDOM.render(</Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
